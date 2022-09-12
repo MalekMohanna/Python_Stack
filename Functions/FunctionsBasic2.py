6110c8437968d0ae99661929a858d2a1d248af29
MalekMohanna <malekmhana@gmail.com>
2022-09-12 16:24:54 +0300

    FunctionsBasic2

---
+++ Functions/FunctionsBasic2.py
@@ -0,0 +1,39 @@
+# def countdown(num):
+#     z=[]
+#     for x in range(num,-1,-1):   first task
+#         z.append(x)
+#     return z
+
+# print(countdown(6))
+
+# def myfunc(mylist):
+#     print(mylist[0])
+#     return(mylist[1])  second task
+# mylist=[4,7]
+# print(myfunc(mylist))
+
+# def addition(mylist):
+#     return(mylist[0]+(len(mylist)))  third task
+# a=[4,5,8]
+# print(addition(a))
+
+# def compare(w):
+#     newarray=[]
+#     for x in range(0,len(w)):
+#         if x==2:
+#             continue
+#         elif w[x]>w[2]:
+#             newarray.append(w[x])  fourth task
+#     print(w[2])
+#     return (newarray)
+# t=[3,8,5,6,9,19,2]
+# print(compare(t))
+
+# def myfunc(length,value):
+#     newlist=[]
+#     for x in range(length):
+#         newlist.append(value)   fifth task
+#     return newlist 
+# x=4
+# y=7
+# print(myfunc(x,y))
