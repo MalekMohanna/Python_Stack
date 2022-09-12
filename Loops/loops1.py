c34f6542a14a5d531c0236b8b069d1730fd300ca
MalekMohanna <malekmhana@gmail.com>
2022-09-12 13:27:38 +0300

    loops1

---
+++ Loops/loops1.py
@@ -0,0 +1,28 @@
+# for x in range(0,151):
+#     print(x)
+
+# for y in range(5,1005,5):
+#     print(y)
+
+# for z in range (1,101,1):
+#     if z%10==0:
+#         print("Coding Dojo")
+#     elif z%5==0:
+#         print("Coding")
+#     else :
+#         print(z)
+
+# sum=0
+# for t in range (0,5000):
+#     sum=sum+t
+# print(sum)
+
+# for h in range(2018,4,-4):
+#     print(h)
+
+# lowNum = 1
+# highNum = 20
+# mult = 4
+# for k in range(lowNum, highNum, 1):
+#     if k%4==0:
+#         print(k)
