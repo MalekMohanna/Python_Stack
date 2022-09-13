4c81d851c8408aef46f579a52f78c46ce02e427a
MalekMohanna <malekmhana@gmail.com>
2022-09-13 13:35:51 +0300

    Users with chain

--- Users/Users.py
+++ Users/Users.py
@@ -5,30 +5,23 @@ class User:
         self.account_balance = 0
     def make_deposit(self,amount):
         self.account_balance+=amount
+        return self
     def make_withdrawal(self,amount):
         self.account_balance-=amount
+        return self
     def make_transfer(self,user,amount):
         self.make_withdrawal(amount)
         user.make_deposit(amount)
+        return self
+    def print_info(self):
+        print(self.name,self.account_balance)
+        return self
 
 karam=User("Karam Taha","karam@gmail.com")
 malik=User("Malik Mohanna","malik@gmail.com")
 ahmad=User("Ahmad Taha","ahmad@gmail.com")
 
-karam.make_deposit(150)
-karam.make_deposit(1500)
-karam.make_deposit(120)
-karam.make_withdrawal(1000)
-malik.make_deposit(2320)
-malik.make_deposit(420)
-malik.make_withdrawal(500)
-malik.make_withdrawal(670)
-ahmad.make_deposit(1330)
-ahmad.make_withdrawal(124)
-ahmad.make_withdrawal(325)
-ahmad.make_withdrawal(345)
-malik.make_transfer(karam,300)
+malik.make_deposit(2320).make_deposit(420).make_withdrawal(500).make_withdrawal(670).make_transfer(karam,600).print_info()
+karam.make_deposit(150).make_deposit(1500).make_deposit(120).make_withdrawal(1000).print_info()
+ahmad.make_deposit(1330).make_withdrawal(124).make_withdrawal(325).make_withdrawal(345).print_info()
 
-print(karam.name,karam.account_balance)
-print(malik.name,malik.account_balance)
-print(ahmad.name,ahmad.account_balance)
